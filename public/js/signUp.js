ac73e4bfb53f27c7b138e2279dee094a67570e1d
antedotee <soniyadav2051982@gmail.com>
2024-09-29 20:23:08 +0530

    2nd commit

--- public/js/signUp.js
+++ public/js/signUp.js
@@ -1,30 +1,26 @@
+import axios from "axios";
 
-import axios from 'axios';
+import { showAlert } from "./alerts";
 
-import { showAlert } from './alerts';
-
-export const signUp = async (name, email , password , passwordConfirm) => {
-
+export const signUp = async (name, email, password, passwordConfirm) => {
   try {
     const res = await axios({
-      method: 'POST',
-      url: '/api/v1/users/signup',
+      method: "POST",
+      url: "/api/v1/users/signup",
       data: {
         name,
         email,
         password,
         passwordConfirm,
-      }
+      },
     });
-    if (res.data.status === 'success'){
-      showAlert('success', 'Sign up successfully');
-      window.setTimeout(() =>{
-        location.assign('/')
+    if (res.data.status === "success") {
+      showAlert("success", "Sign up successfully");
+      window.setTimeout(() => {
+        location.assign("/");
       }, 500);
     }
-  }
-  catch (err) {
-    showAlert('error',err.response.data.message);
+  } catch (err) {
+    showAlert("error", err.response.data.message);
   }
 };
-``
--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-const dotenv = require('dotenv');
-process.on('uncaughtException', err => {
-    console.log('UNHANDIER REJECTION! (: Shutting down');
-    console.log(err.name , err.message);
-    process.exit(1);
+const dotenv = require("dotenv");
+process.on("uncaughtException", (err) => {
+  console.log("UNHANDIER REJECTION! (: Shutting down");
+  console.log(err.name, err.message);
+  process.exit(1);
 });
 dotenv.config({
-    path: 'config.env'
+  path: "config.env",
 });
-const app = require('./app.js');
-const mongoose = require('mongoose');
-
+const app = require("./app.js");
+const mongoose = require("mongoose");
 
 // Secure way
-const DB = process.env.DATABASE.replace('<password>', process.env.DATABASE_PASSWORD);
-mongoose.connect(DB, {
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-    useFindAndModify: false
-}).then(() => {
-    console.log('DB connection successful');
-});
-
-
-
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("DB connection successful");
+  });
 
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => {
-    console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...`);
 });
-
 
 // process.on('unhandledRejection', err => {
 //     console.log(err.name, err.message);
@@ -39,24 +40,23 @@ const server = app.listen(port, () => {
 //     });
 // });
 
-
-process.on('unhandledRejection', (err) => {
-    console.error('Unhandled Rejection:', err);
-    // Perform any necessary cleanup and error handling
-    // ...
-    process.exit(1); // Terminate the process with a non-zero exit code
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+  // Perform any necessary cleanup and error handling
+  // ...
+  process.exit(1); // Terminate the process with a non-zero exit code
 });
 
-process.on('uncaughtException', (err) => {
-    console.error('Uncaught Exception:', err);
-    // Perform any necessary cleanup and error handling
-    // ...
-    process.exit(1); // Terminate the process with a non-zero exit code
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception:", err);
+  // Perform any necessary cleanup and error handling
+  // ...
+  process.exit(1); // Terminate the process with a non-zero exit code
 });
 
-process.on('SIGTERM' , () => {
-   console.log('SIGTERM RECEIVED, SHUTTING DOWN');
-   server.close(() =>{
-       console.log('Process terminated successfully');
-   });
+process.on("SIGTERM", () => {
+  console.log("SIGTERM RECEIVED, SHUTTING DOWN");
+  server.close(() => {
+    console.log("Process terminated successfully");
+  });
 });
