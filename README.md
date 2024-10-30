86c13646aef2265fa04c10707699ba78c87854d5
antedotee <soniyadav2051982@gmail.com>
2024-10-31 00:45:11 +0530

    used resend instead of sendgrid, updated Readme

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## An Awesome Tour Booking Site Built on Top of NodeJS
 
+<!-- ### Key Features
 
-### Key Features
 - Authentication and Authorization
 - Sign up with Email Verification
 - Login and Logout
@@ -12,8 +12,100 @@
 - Interactive Tour Map with User Reviews and Ratings
 - User Profile Customization
 - Secure Credit Card Payment
-- Dark Mode for Enhanced User Experience
+- Dark Mode for Enhanced User Experience -->
+
+### Overview
+
+This repository contains the source code for a booking application built using Express.js and MongoDB. The app allows users to manage tours, make bookings, and leave reviews while providing an admin interface for managing users and tours.
+
+### Features
+
+- User authentication using JWT tokens.
+- Ability to create, update, and delete bookings.
+- Review management for tours.
+- Admin functionality to manage users and tours.
+  S- ecure token-based authentication for API endpoints.
+- MongoDB for storing user data, tours, bookings, and reviews.
+
+<!-- ### Prerequisites
+
+Before you begin, ensure you have the following dependencies installed:
+
+- Node.js
+- npm
+- MongoDB
+!-->
+
+### Installation
+
+```
+git clone https://github.com/antedotee/Natours
+npm i
+```
+
+<!-- Create a .env file and set the following environment variables:
+env
+Copy code
+MONGO_URL=your_mongodb_connection_string
+PORT=5000 -->
+
+### Entry Point
+
+The entry point for the backend application is backend/server.js.
+
+### Running the Application
+
+`npm run start`
+
+<!-- This application uses MongoDB to store data, including models for users, tours, bookings, and reviews. -->
+
+### API Endpoints
+
+#### User Authentication
+
+- POST /api/auth/signup: Sign up a new user.
+- POST /api/auth/login: Log in as a user and generate a JWT token.
+- GET /api/user/me: Get current user's details.
+- PATCH /api/user/updateMe: Update current user's data.
+- DELETE /api/user/deleteMe: Delete current user's account.
+
+#### Booking Management
+
+- GET /api/bookings: Get all bookings (Admin only).
+- POST /api/bookings: Create a new booking.
+- GET /api/bookings/:id: Get details of a specific booking.
+- PATCH /api/bookings/:id: Update a specific booking (Admin only).
+- DELETE /api/bookings/:id: Delete a specific booking (Admin only).
+
+#### Tour Management
+
+- GET /api/tours: Get a list of all tours.
+- POST /api/tours: Create a new tour (Admin only).
+- GET /api/tours/:id: Get details of a specific tour.
+- PATCH /api/tours/:id: Update a specific tour (Admin only).
+- DELETE /api/tours/:id: Delete a specific tour (Admin only).
+
+#### Review Management
+
+- GET /api/tours/:tourId/reviews: Get all reviews for a specific tour.
+- POST /api/tours/:tourId/reviews: Create a review for a specific tour (User only).
+- PATCH /api/reviews/:id: Update a specific review (Admin/User).
+- DELETE /api/reviews/:id: Delete a specific review (Admin/User).
+
+### Technologies Used
+
+- Express.js: A web application framework for Node.js.
+- MongoDB: A NoSQL database for storing application data.
+- Mongoose: An ODM library for MongoDB and Node.js.
+- JWT: For user authentication and secure API access.
+- Resend : For sending emails
+- Stripe : For payment of tours
 
 ### Deployment
+
 The website is deployed with Git into render:
 (https://natours-fonh.onrender.com/)
+
+### License
+
+This project is licensed under the MIT License.
--- utils/email.js
+++ utils/email.js
@@ -1,101 +1,54 @@
-const nodemailer = require('nodemailer');
-const pug = require('pug');
-const { htmlToText } = require('html-to-text');
+const { Resend } = require("resend");
+const pug = require("pug");
+const { htmlToText } = require("html-to-text");
+
+// Initialize Resend with your API key
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 // new Email(user , url).sendWelcome();
 module.exports = class Email {
-  // Function will be going to run when new object created from this class
   constructor(user, url) {
     this.to = user.email;
-    this.firstName = user.name.split(' ')[0];
+    this.firstName = user.name.split(" ")[0];
     this.url = url;
     this.from = `Kartik Yadav <${process.env.EMAIL_FROM}>`;
   }
 
-  newTransport() {
-    if (process.env.NODE_ENV === 'production') {
-      return nodemailer.createTransport({
-        // host : 'smtp-relay.sendinblue.com',
-        // port:  '587',
-        service: 'SendinBlue',
-        auth: {
-          user: process.env.SENDINBLUE_USERNAME,
-          pass: process.env.SENDINBLUE_PASSWORD
-        }
-      });
-    }
-
-    return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      // service : "Gmail",
-      // Activate in gmail "less secure app " options
-      auth: {
-        user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      }
-    });
-  }
-  // Send the actual email
   async send(template, subject) {
-    // 1) render HTML based on the template
+    // 1) Render HTML based on the template
     const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
       firstName: this.firstName,
       url: this.url,
-      subject
+      subject,
     });
+
     // 2) Define the email options
     const mailOptions = {
       from: this.from,
       to: this.to,
       subject,
       html,
-      text: htmlToText(html)
-      // text: htmlToText.fromString(html)
-      //html:
+      text: htmlToText(html),
     };
 
-    // 3) Create a transport and send email
-    await this.newTransport().sendMail(mailOptions);
+    // 3) Send email using Resend
+    await resend.emails.send({
+      from: mailOptions.from,
+      to: mailOptions.to,
+      subject: mailOptions.subject,
+      html: mailOptions.html,
+      text: mailOptions.text,
+    });
   }
 
-  // this is useful so we can create multiple different subjects
   async sendWelcome() {
-    await this.send('welcome', 'Welcome to the Natours Family');
+    await this.send("welcome", "Welcome to the Natours Family");
   }
 
   async sendPasswordReset() {
     await this.send(
-      'passwordReset',
-      'Your password reset token (valid for only 10 minutes)'
+      "passwordReset",
+      "Your password reset token (valid for only 10 minutes)"
     );
   }
 };
-
-// const sendEmail = async options => {
-//   1) Create a transporter
-//   const transporter = nodemailer.createTransport({
-//
-//     host: process.env.EMAIL_HOST,
-//     port: process.env.EMAIL_PORT,
-//     //service : "Gmail",
-//     // Activate in gmail "less secure app " options
-//     auth: {
-//       user: process.env.EMAIL_USERNAME,
-//       pass: process.env.EMAIL_PASSWORD
-//     }
-//   });
-//   2) Define the email options
-//   const mailOptions = {
-//     from: 'Kartik Yadav <kartik.y23csai@nst.rishihood.edu.in>',
-//     to: options.email,
-//     subject: options.subject,
-//     text: options.message
-//     //html:
-//   };
-//
-//   3) Actually send the email
-//   await transporter.sendMail(mailOptions);
-// };
-//
-// module.exports = sendEmail;
