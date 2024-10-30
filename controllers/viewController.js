33fdda67401d90ed910a733a9d460fa43584562a
antedotee <soniyadav2051982@gmail.com>
2024-10-30 21:45:55 +0530

    fixed the image issue

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,74 +1,72 @@
-const Tour = require('../models/tourModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('./../utils/appError');
-const User = require('../models/userModel');
-const Booking = require('../models/bookingModel');
-
+const Tour = require("../models/tourModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("./../utils/appError");
+const User = require("../models/userModel");
+const Booking = require("../models/bookingModel");
 
 exports.getOverview = catchAsync(async (req, res) => {
   // 1) Get tour data from collection
   const tours = await Tour.find();
   // 2) Build Template
   // 3) Render that template using tour data from 1
-  res.status(200).render('overview', {
-    title: 'All Tours',
-    tours
+  res.status(200).render("overview", {
+    title: "All Tours",
+    tours,
   });
 });
-
 
 // Details page
 exports.getTour = catchAsync(async (req, res, next) => {
   // 1) get the data, for the requested tour (including review and guides)
   const tourSlug = req.params.tourSlug;
   const tour = await Tour.findOne({ slug: tourSlug }).populate({
-    path: 'reviews',
+    path: "reviews",
     populate: {
-      path: 'user',
-      fields: 'review rating user'
-    }
+      path: "user",
+      fields: "review rating user",
+    },
   });
 
   if (!tour) {
-    return next(new AppError('There is no tour with that name', 404));
+    return next(new AppError("There is no tour with that name", 404));
   }
   // 2) Build Template
   // 3) Render that template using tour data from 1
-  res.status(200).render('tour', {
-    title: tourSlug + ' Tour',
-    tour
+  res.status(200).render("tour", {
+    title: tourSlug + " Tour",
+    tour,
   });
 });
 
 // Signup page
 
-exports.getSignUp = (req , res) =>{
-   res.status(200).render('signup', {
-     title : 'Sign Up',
-   })
-}
+exports.getSignUp = (req, res) => {
+  res.status(200).render("signup", {
+    title: "Sign Up",
+  });
+};
 
-exports.getResetPassword = (req , res) =>{
-  res.status(200).render('resetpassword', {
-    title : 'Reset Password',
-  })
-}
+exports.getResetPassword = (req, res) => {
+  res.status(200).render("resetpassword", {
+    title: "Reset Password",
+  });
+};
 
-exports.getForgotPassword = (req , res) =>{
-  res.status(200).render('forgotpassword', {
-    title : 'Forget Password',
-  })
-}
+exports.getForgotPassword = (req, res) => {
+  res.status(200).render("forgotpassword", {
+    title: "Forget Password",
+  });
+};
 // Login page
 exports.getLoginForm = (req, res) => {
-  res.status(200).render('login', {
-    title: 'Log into you account'
+  res.status(200).render("login", {
+    title: "Log into you account",
   });
 };
 
 exports.getAccount = (req, res) => {
-  res.status(200).render('account', {
-    title: 'Your account'
+  res.status(200).render("account", {
+    title: "Your account",
   });
 };
 
@@ -78,30 +76,32 @@ exports.getMyTours = catchAsync(async (req, res, next) => {
   const bookings = await Booking.find({ user: req.user.id });
   console.log(bookings);
   // 2) Find Tours with the returned ID'S
-  const tourIDs = bookings.map(el => {return el.tour});
+  const tourIDs = bookings.map((el) => {
+    return el.tour;
+  });
   console.log(tourIDs);
   const tours = await Tour.find({ _id: { $in: tourIDs } });
 
-  res.status(200).render('overview' , {
-     title : 'My Tours',
-     tours,
+  res.status(200).render("overview", {
+    title: "My Tours",
+    tours,
   });
 });
 
-
 exports.updateUserData = catchAsync(async (req, res, next) => {
-  // const updatedUser = await User.findByIdAndUpdate(req.user.id, {
+  // const updatedUser = await User.findByIdAndUpdate(
+  //   req.user.id,
+  //   {
   //     name: req.body.name,
-  //     email: req.body.email
+  //     email: req.body.email,
   //   },
   //   {
   //     new: true,
-  //     runValidators: true
+  //     runValidators: true,
   //   }
   // );
-  res.status(200).render('account', {
-    title: 'Your account',
-    user: req.user
+  res.status(200).render("account", {
+    title: "Your account",
+    user: req.user,
   });
-
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,38 +1,54 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const {getAllUsers , getUser , updateUser , deleteUser , createUser , updateMe , deleteMe , getMe , uploadUserPhoto , resizeUserPhoto} = require('./../controllers/userController');
-const{signup , login , forgotPassword , resetPassword , updatePassword , protect ,restrictTo ,logout} = require('./../controllers/authController');
+const {
+  getAllUsers,
+  getUser,
+  updateUser,
+  deleteUser,
+  createUser,
+  updateMe,
+  deleteMe,
+  getMe,
+  uploadUserPhoto,
+  resizeUserPhoto,
+} = require("./../controllers/userController");
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  protect,
+  restrictTo,
+  logout,
+} = require("./../controllers/authController");
 
+router.get("/me", protect, getMe, getUser);
 
-router.get('/me' , protect , getMe , getUser);
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/logout", logout);
 
-router.post('/signup' , signup);
-router.post('/login' , login);
-router.get('/logout' , logout);
-
-
-
-
-
-router.post('/forgotPassword' , forgotPassword);
-router.patch('/resetPassword/:token' , resetPassword);
+router.post("/forgotPassword", forgotPassword);
+router.patch("/resetPassword/:token", resetPassword);
 
 router.use(protect);
 
-router.patch('/updateMyPassword' , updatePassword);
-router.patch('/updateMe',  uploadUserPhoto ,resizeUserPhoto  ,  updateMe);
-router.delete('/deleteMe' , deleteMe);
+router.patch("/updateMyPassword", updatePassword);
+router.patch("/updateMe", uploadUserPhoto, resizeUserPhoto, updateMe);
+
+router.delete("/deleteMe", deleteMe);
 
 // Rest format
 
-router.use(restrictTo('admin'));
+router.use(restrictTo("admin"));
 router
-  .route('/')
+  .route("/")
   .get(getAllUsers)
   .post(createUser);
 router
-  .route('/:id')
+  .route("/:id")
   .get(getUser)
   .patch(updateUser)
   .delete(deleteUser);
